8461e112ffdb34a87203f90c12a054a871e04145
Грона Оля <grona.hell@gmail.com>
2024-05-09 17:12:08 +0300

    add main markup

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+// import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    
+    <h1>Eurovision BINGO!</h1>
 
-setupCounter(document.querySelector('#counter'))
+    <p>
+      Mark off a box every time it happens on screen during the show - and reward yourself with a sip of your chosen drink!
+    </p>
+    <img src="./images/esc-sweden.png" alt="esc sweden logo">
+  </div>
+`;
+
+// setupCounter(document.querySelector("#counter"));
